11915240eb95189dd977c5ebf78b0de7e8001b76
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-02-13 14:34:24 +0200

    Add new event functuonality implemented to the event controller.

--- Homies/Controllers/EventController.cs
+++ Homies/Controllers/EventController.cs
@@ -1,9 +1,11 @@
 ﻿using Homies.Data;
+using Homies.Data.Models;
 using Homies.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Globalization;
+using System.Security.Claims;
 
 namespace Homies.Controllers
 {
@@ -15,6 +17,8 @@ namespace Homies.Controllers
         {
             this.data = context;
         }
+
+        [HttpGet]
         public async Task<IActionResult> All()
         {
 
@@ -31,6 +35,89 @@ namespace Homies.Controllers
                 .ToListAsync();
 
             return View(events);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var types = await GetTypesAsync();
+
+            var eventForm = new EventFormModel()
+            {
+                Types = types
+            };
+
+            return View(eventForm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(EventFormModel model)
+        {
+            DateTime parsedStart = DateTime.Now;
+            DateTime parsedEnd = DateTime.Now;
+
+            if (!DateTime.TryParseExact(
+                model.Start,
+                DataConstants.DateFormat,
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None, 
+                out parsedStart))
+            {
+                ModelState.AddModelError(nameof(model.Start), $"Invalid Date. Format must be: {DataConstants.DateFormat}");
+            }
+
+            if (!DateTime.TryParseExact(
+                model.End,
+                DataConstants.DateFormat,
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out parsedEnd))
+            {
+                ModelState.AddModelError(nameof(model.End), $"Invalid Date. Format must be: {DataConstants.DateFormat}");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                model.Types=await GetTypesAsync();
+                return View(model);
+            }
+
+            var entity = new Event()
+            {
+                Name = model.Name,
+                Description = model.Description,
+                CreatedOn = DateTime.Now,
+                Start = parsedStart,
+                End = parsedEnd,
+                TypeId = model.TypeId,
+                OrganiserId = GetUserIdAsync()
+            };
+
+            await data.AddAsync(entity);
+            await data.SaveChangesAsync();
+
+            return RedirectToAction(nameof(All));
+        }
+
+
+
+        private string GetUserIdAsync()
+        {
+            return User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        private async Task<IList<TypeViewModel>> GetTypesAsync()
+        {
+            var types = await data.Types
+                .AsNoTracking()
+                .Select(e => new TypeViewModel()
+                {
+                    Id = e.Id,
+                    Name = e.Name
+                })
+                .ToListAsync();
+
+            return types;
         }
     }
 }
---
+++ Homies/Models/EventFormModel.cs
@@ -0,0 +1,28 @@
+﻿using Homies.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace Homies.Models
+{
+    public class EventFormModel
+    {
+        [Required]
+        [StringLength(DataConstants.EventNameMaxLength, MinimumLength = DataConstants.EventNameMinLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(DataConstants.EventDescriptionMaxLength, MinimumLength = DataConstants.EventDescriptionMinLength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string Start { get; set; } = string.Empty;
+
+        [Required]
+        public string End { get; set; } = string.Empty;
+
+        [Required]
+        public int TypeId { get; set; }
+
+        public string OrganiserId { get; set; } = string.Empty;
+        public ICollection<TypeViewModel> Types { get; set; } = new List<TypeViewModel>();
+    }
+}
