7134fd9f7f1acc91f275f34fd278514f7d60e9ba
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-02-13 10:49:47 +0200

    Add project files.

---
+++ Homies/Data/DataConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace Homies.Data
+{
+    public static class DataConstants
+    {
+        //Even constants
+        public const int EventNameMinLength = 5;
+        public const int EventNameMaxLength = 20;
+
+        public const int EventDescriptionMinLength = 15;
+        public const int EventDescriptionMaxLength = 150;
+
+
+        //Type constants
+
+        //Date format
+        public const string DateFormat = "yyyy-MM-dd H:mm";
+    }
+}
---
+++ Homies/Data/Models/Event.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Homies.Data.Models
+{
+    /// <summary>
+    /// Event database entity
+    /// </summary>
+    [Comment("Event entity")]
+    public class Event
+    {
+        /// <summary>
+        /// Event identifier
+        /// </summary>
+        [Key]
+        [Comment("Event identifier")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Event name
+        /// </summary>
+        [Required]
+        [MaxLength(DataConstants.EventNameMaxLength)]
+        [Comment("Event name")]
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Event description
+        /// </summary>
+        [Required]
+        [MaxLength(DataConstants.EventDescriptionMaxLength)]
+        [Comment("Event description")]
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Event organiser identifier
+        /// </summary>
+        [Required]
+        [Comment("Event organiser identifier")]
+        public int OrganiserId { get; set; }
+
+        /// <summary>
+        /// Event organiser
+        /// </summary>
+        [Required]
+        [Comment("Event organiser")]
+        public IdentityUser Organiser { get; set; } = null!;
+
+        /// <summary>
+        /// Date and time event is created
+        /// </summary>
+        [Required]
+        [Comment("Date and time event is created")]
+        public DateTime CreatedOn { get; set; }
+
+        /// <summary>
+        /// Date and time event starts
+        /// </summary>
+        [Required]
+        [Comment("Date and time event starts")]
+        public DateTime Start { get; set; }
+
+        /// <summary>
+        /// Date and time event ends
+        /// </summary>
+        [Required]
+        [Comment("Date and time event ends")]
+        public DateTime End { get; set; }
+
+        /// <summary>
+        /// Event's type identifier
+        /// </summary>
+        [Required]
+        [Comment("Event's type identifier")]
+        public int TypeId { get; set; }
+
+        /// <summary>
+        /// Event's type
+        /// </summary>
+        [Required]
+        [Comment("Event's type")]
+        public Type Type { get; set; } = null!;
+
+        /// <summary>
+        /// EventsParticipants
+        /// </summary>
+        public ICollection<EventParticipant> EventsParticipants { get; set; } = new List<EventParticipant>();
+    }
+}
